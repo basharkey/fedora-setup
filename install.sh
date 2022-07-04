cedeac5dc9a9264170f89be6303eddb93628538e
Bradley Sharkey <15859442+basharkey@users.noreply.github.com>
2022-07-04 15:34:51 -0400

    moved playbook dependencies to virtualenv

--- install.sh
+++ install.sh
@@ -1,4 +1,9 @@
-#!/bin/bash
-sudo dnf install -y ansible
+#!/usr/bin/env bash
+sudo dnf install -y ansible python3 python3-virtualenv
 ansible-galaxy install -r requirements.yml
+
+virtualenv .
+source bin/activate
+bin/pip3 install lxml selinux
+
 ansible-playbook main.yml -i hosts.yml -e @vault.yml --ask-vault-pass
