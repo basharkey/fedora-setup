0f6b09e82c980a34036ac457dbe093b3de3b83da
basharkey <15859442+basharkey@users.noreply.github.com>
2022-07-04 17:58:18 -0400

    added playbook dependencies back to main

--- install.sh
+++ install.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
-sudo dnf install -y ansible python3 python3-virtualenv
+sudo dnf install -y ansible
 ansible-galaxy install -r requirements.yml
-
-virtualenv .
-source bin/activate
-bin/pip3 install lxml selinux
 
 ansible-playbook main.yml -i hosts.yml -e @vault.yml --ask-vault-pass
